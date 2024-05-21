1b08d130464f4bcd178ec85df128082a1d95eb17
tatsabong hanniel <tatsabonghanniel@gmail.com>
2024-05-21 18:19:34 +0100

    Use Yup and Formik to validate form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,31 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
+const validators = {
+  first_name: Yup.string().required("Le prénom est requis"),
+  last_name: Yup.string().required("Le nom de famille est requis"),
+  email: Yup.string()
+    .email("Adresse email invalide")
+    .required("L'email est requis"),
+  gender: Yup.string().required("Le sexe est requis"),
+};
 
 const initialUsers = [
   {
     id: 1,
-    firstName: "Hanniel",
-    lastName: "TSASSE",
+    first_name: "Hanniel",
+    last_name: "TSASSE",
     email: "hanniel.tsasse@gmail.com",
     gender: "M",
     avatar: "https://via.placeholder.com/150",
   },
   {
     id: 2,
-    firstName: "Josué",
-    lastName: "FOUDJI",
+    first_name: "Josué",
+    last_name: "FOUDJI",
     email: "josue.foudji@yahoo.fr",
     gender: "F",
     avatar: "https://via.placeholder.com/150",
@@ -30,14 +41,63 @@ function LoadingOverlay() {
   );
 }
 
-export default function Example() {
+export default function HomePage() {
   const [users, setUsers] = useState(initialUsers);
+  const [errors, setErrors] = useState<Record<string, any>>({});
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [currentUser, setCurrentUser] = useState<any>(null);
   const [isEditing, setIsEditing] = useState(false);
   const [loading, setLoading] = useState(false);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [error, setError] = useState<string>("");
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    validationSchema: Yup.object().shape(validators),
+    initialValues: currentUser || {
+      first_name: "",
+      last_name: "",
+      email: "",
+      gender: "",
+    },
+    onSubmit: async (values) => {
+      setLoading(true);
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+
+        if (isEditing) {
+          setUsers(
+            users.map((user: any) =>
+              user.id === currentUser!.id
+                ? { ...currentUser, ...values, avatar: currentUser.avatar }
+                : user
+            )
+          );
+        } else {
+          setUsers([
+            ...users,
+            {
+              ...values,
+              id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
+              avatar: selectedFile
+                ? URL.createObjectURL(selectedFile)
+                : "https://via.placeholder.com/150",
+            },
+          ]);
+        }
+        closeModal();
+      } catch (error: any) {
+        const newErrors: any = {};
+        error.inner.forEach((err: any) => {
+          newErrors[err.path] = err.message;
+        });
+        setErrors(newErrors);
+      } finally {
+        setLoading(false);
+      }
+    },
+  });
 
   const openModal = (user: any = null) => {
     setCurrentUser(user);
@@ -50,6 +110,7 @@ export default function Example() {
     setCurrentUser(null);
     setIsEditing(false);
     setSelectedFile(null);
+    setError("");
   };
 
   const openDeleteModal = (user: any) => {
@@ -70,28 +131,6 @@ export default function Example() {
         avatar: URL.createObjectURL(e.target.files[0]),
       });
     }
-  };
-
-  const handleSave = async () => {
-    setLoading(true);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    if (isEditing) {
-      setUsers(
-        users.map((user: any) =>
-          user.id === currentUser!.id
-            ? { ...currentUser, avatar: currentUser.avatar }
-            : user
-        )
-      );
-    } else {
-      setUsers([
-        ...users,
-        { ...currentUser, id: users.length + 1, avatar: currentUser.avatar },
-      ]);
-    }
-    setLoading(false);
-    closeModal();
   };
 
   const handleDelete = async () => {
@@ -134,11 +173,11 @@ export default function Example() {
                   height={150}
                   className="w-[150px] h-[150px] rounded-full mb-4"
                   src={user.avatar}
-                  alt={`${user.firstName} ${user.lastName} profile`}
+                  alt={`${user.first_name} ${user.last_name} profile`}
                 />
                 <div className="text-center">
                   <h3 className="text-lg font-semibold text-white">
-                    {user.firstName} {user.lastName}
+                    {user.first_name} {user.last_name}
                   </h3>
                   <p className="text-gray-300">{user.email}</p>
                   <p className="text-gray-300">
@@ -167,80 +206,127 @@ export default function Example() {
 
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
+          <form
+            onSubmit={formik.handleSubmit}
+            className="bg-white p-6 rounded-lg shadow-lg"
+          >
             <h2 className="text-xl font-semibold mb-4">
               {isEditing ? "Mise à jour d'un" : "Ajouter un"} utilisateur
             </h2>
-            <input
-              type="text"
-              placeholder="Prénom"
-              value={currentUser?.firstName || ""}
-              onChange={(e) =>
-                setCurrentUser({ ...currentUser, firstName: e.target.value })
-              }
-              className="block w-full px-4 py-2 mb-4 border rounded-md"
-            />
-            <input
-              type="text"
-              placeholder="Nom"
-              value={currentUser?.lastName || ""}
-              onChange={(e) =>
-                setCurrentUser({ ...currentUser, lastName: e.target.value })
-              }
-              className="block w-full px-4 py-2 mb-4 border rounded-md"
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              value={currentUser?.email || ""}
-              onChange={(e) =>
-                setCurrentUser({ ...currentUser, email: e.target.value })
-              }
-              className="block w-full px-4 py-2 mb-4 border rounded-md"
-            />
-            <select
-              value={currentUser?.gender || ""}
-              onChange={(e) =>
-                setCurrentUser({ ...currentUser, gender: e.target.value })
-              }
-              className="block w-full px-4 py-2 mb-4 border rounded-md"
-            >
-              <option value="">Sélectionner le sexe</option>
-              <option value="M">Masculin</option>
-              <option value="F">Féminin</option>
-            </select>
-            <input
-              type="file"
-              onChange={handleFileChange}
-              className="block w-full px-4 py-2 mb-4 border rounded-md"
-            />
-            {currentUser?.avatar && (
-              <div className="mb-4">
-                <Image
-                  width={150}
-                  height={150}
-                  className="w-[150px] h-[150px] rounded-full"
-                  src={currentUser.avatar}
-                  alt="Profile preview"
-                />
+            {error && (
+              <div className="w-full px-3 py-3 mt-4 text-red-700 rounded-md bg-red-50">
+                {error}
               </div>
             )}
+            <div className="mb-4">
+              <input
+                type="text"
+                placeholder="Prénom"
+                name="first_name"
+                value={formik.values.first_name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={`block w-full px-4 py-2 mb-1 border rounded-md ${
+                  formik.touched.first_name && formik.errors.first_name
+                    ? "border-red-500"
+                    : ""
+                }`}
+              />
+              {formik.touched.first_name && formik.errors.first_name && (
+                <div className="text-red-500">{formik.errors.first_name}</div>
+              )}
+            </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                placeholder="Nom"
+                name="last_name"
+                value={formik.values.last_name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={`block w-full px-4 py-2 mb-1 border rounded-md ${
+                  formik.touched.last_name && formik.errors.last_name
+                    ? "border-red-500"
+                    : ""
+                }`}
+              />
+              {formik.touched.last_name && formik.errors.last_name && (
+                <div className="text-red-500">{formik.errors.last_name}</div>
+              )}
+            </div>
+            <div className="mb-4">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                className={`block w-full px-4 py-2 mb-1 border rounded-md ${
+                  formik.touched.email && formik.errors.email
+                    ? "border-red-500"
+                    : ""
+                }`}
+              />
+              {formik.touched.email && formik.errors.email && (
+                <div className="text-red-500">{formik.errors.email}</div>
+              )}
+            </div>
+            <div className="mb-4">
+              <select
+                className={`block w-full px-4 py-2 mb-1 border rounded-md ${
+                  formik.touched.gender && formik.errors.gender
+                    ? "border-red-500"
+                    : ""
+                }`}
+                name="gender"
+                value={formik.values.gender}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              >
+                <option value="">Sélectionner le sexe</option>
+                <option value="M">Homme</option>
+                <option value="F">Femme</option>
+              </select>
+              {formik.touched.gender && formik.errors.gender && (
+                <div className="text-red-500">{formik.errors.gender}</div>
+              )}
+            </div>
+            <div className="mb-4">
+              <input
+                type="file"
+                onChange={handleFileChange}
+                className="block w-full px-4 py-2 mb-1 border rounded-md"
+              />
+              {currentUser?.avatar && (
+                <div className="mb-4">
+                  <Image
+                    width={150}
+                    height={150}
+                    className="w-[150px] h-[150px] rounded-full"
+                    src={currentUser.avatar}
+                    alt="Profile preview"
+                  />
+                </div>
+              )}
+            </div>
             <div className="flex justify-end space-x-2">
               <button
                 onClick={closeModal}
+                type="button"
                 className="bg-gray-500 text-white px-4 py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 transition"
               >
                 Annuler
               </button>
               <button
-                onClick={handleSave}
+                type="submit"
                 className="bg-green-600 text-white px-4 py-2 rounded-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-opacity-50 transition"
                 disabled={loading}
               >
                 {loading ? "En cours..." : isEditing ? "Modifier" : "Ajouter"}
               </button>
             </div>
-          </div>
+          </form>
         </div>
       )}
 
@@ -251,8 +337,8 @@ export default function Example() {
               Confirmer la suppression
             </h2>
             <p>
-              Voulez-vous vraiment supprimer {currentUser?.firstName}{" "}
-              {currentUser?.lastName} ?
+              Voulez-vous vraiment supprimer {currentUser?.first_name}{" "}
+              {currentUser?.last_name} ?
             </p>
             <div className="flex justify-end space-x-2 mt-4">
               <button
